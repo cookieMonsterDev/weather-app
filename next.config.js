f0322138da9242a99724a057052bd2a73e051ba9
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-02-05 00:41:59 +0200

    fix: CORS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-
-module.exports = {
+const nextConfig = {
   env: {
     MONGO_URL: process.env.MONGO_URL,
     WEATHER_BASE_URL: process.env.WEATHER_BASE_URL,
@@ -10,26 +8,6 @@ module.exports = {
   },
 }
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ]
-  },
-};
+module.exports = nextConfig
+
+
