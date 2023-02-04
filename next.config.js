55db4e31e88e379f7aef2c1266fab816cac0ae52
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-02-05 00:31:17 +0200

    fix: change http protocol

--- next.config.js
+++ next.config.js
@@ -9,3 +9,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+module.exports = {
+  async rewrites() {
+      return [
+        {
+          source: '*',
+          destination: '*',
+        },
+      ]
+    },
+};
