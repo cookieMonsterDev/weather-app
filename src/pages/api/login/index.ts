145bf26d397e5575c2ab203293a729dbd8f6b3af
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-02-05 08:47:22 +0200

    chore: add vercel config

--- src/pages/api/login/index.ts
+++ src/pages/api/login/index.ts
@@ -1,26 +1,23 @@
 import { loginAsUserService } from "@/services/LoginAsUserService";
 import type { NextApiRequest, NextApiResponse } from "next";
-import NextCors from "nextjs-cors";
 
 const loginAsUser = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    await NextCors(req, res, {
-      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-      origin: "*",
-      optionsSuccessStatus: 200,
-    });
+  const { method } = req;
 
-    const user = await loginAsUserService(req.body);
+  if (method === "OPTIONS") {
+    try {
+      const user = await loginAsUserService(req.body);
 
-    res.status(200).json({
-      isError: false,
-      res: user,
-    });
-  } catch (error) {
-    res.status(200).send({
-      isError: true,
-      res: error,
-    });
+      res.status(200).json({
+        isError: false,
+        res: user,
+      });
+    } catch (error) {
+      res.status(200).send({
+        isError: true,
+        res: error,
+      });
+    }
   }
 };
 
--- src/pages/api/register/index.ts
+++ src/pages/api/register/index.ts
@@ -1,28 +1,24 @@
-
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createUserService } from "@/services/CreateUserService";
-import NextCors from "nextjs-cors";
 
 const RegisterUser = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    await NextCors(req, res, {
-      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-      origin: "*",
-      optionsSuccessStatus: 200,
-    });
+  const { method } = req;
 
-    const user = await createUserService(req.body);
-    res.status(200).json({
-      isError: false,
-      res: user
-    });
+  if (method === "OPTIONS") {
+    try {
+      const user = await createUserService(req.body);
+
+      res.status(200).json({
+        isError: false,
+        res: user,
+      });
+    } catch (error) {
+      res.status(200).send({
+        isError: true,
+        res: error,
+      });
+    }
   }
-  catch (error) {
-    res.status(200).send({
-      isError: true,
-      res: error
-    });
-  }
-}
+};
 
 export default RegisterUser;
