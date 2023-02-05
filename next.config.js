bf4d4eb9cd334964b09f436bc688303351a5a2a5
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-02-05 08:55:58 +0200

    chore: add vercel config

--- next.config.js
+++ next.config.js
@@ -5,18 +5,13 @@ const nextConfig = {
     WEATHER_BASE_URL: process.env.WEATHER_BASE_URL,
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,
     VERCEL_URL: process.env.VERCEL_URL,
-  }
-};
-
-module.exports = nextConfig;
-
-module.exports = {
+  },
   async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://api.example.com/:path*',
-        },
-      ]
-    },
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.example.com/:path*',
+      },
+    ]
+  },
 };
