20426655fe32bf57c630b29589f39832d5f3c2dc
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-02-05 09:43:08 +0200

    chore: add vercel config

--- src/pages/api/login/index.ts
+++ src/pages/api/login/index.ts
@@ -1,8 +1,11 @@
 import { loginAsUserService } from "@/services/LoginAsUserService";
 import type { NextApiRequest, NextApiResponse } from "next";
+import connectMongo from "utils/connectMongo";
 
 const loginAsUser = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
+    await connectMongo()
+
     const user = await loginAsUserService(req.body);
 
     res.status(200).json({
--- src/pages/api/register/index.ts
+++ src/pages/api/register/index.ts
@@ -1,8 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createUserService } from "@/services/CreateUserService";
+import connectMongo from "utils/connectMongo";
 
 const RegisterUser = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
+      await connectMongo()
+    
       const user = await createUserService(req.body);
 
       res.status(200).json({
